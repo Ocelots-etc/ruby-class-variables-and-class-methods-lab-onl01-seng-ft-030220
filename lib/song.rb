7db2d42109c5ede1548c1625c435de28533ae1ba
Cat Gomez <catherinedgomez@gmail.com>
2020-03-15 08:41:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,28 +2,28 @@ class Song
   attr_accessor :name, :artist, :genre
 # attr_reader :name, :artist, :genre
 
-def prop=(prop1, prop2, prop3)
-  @prop = prop
+def name=(name)
+  @name = name
 end
 
-def prop
-  @prop
+def name
+  @name
 end
-#
-# def artist=(artist)
-#   @artist = artist
-# end
-#
-# def artist
-#   @artist
-# end
-#
-# def genre=(genre)
-#   @genre = genre
-# end
-#
-# def genre
-#   @genre
-# end
+
+def artist=(artist)
+  @artist = artist
+end
+
+def artist
+  @artist
+end
+
+def genre=(genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
 
 end
