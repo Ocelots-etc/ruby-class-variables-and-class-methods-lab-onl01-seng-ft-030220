e7eb254b91c1426200c33799b0272d2c524846db
Cat Gomez <catherinedgomez@gmail.com>
2020-03-15 08:51:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,15 +15,15 @@ end
 # def artist=(artist)
 #   @artist = artist
 # end
-# 
+#
 # def artist
 #   @artist
 # end
-# 
+#
 # def genre=(genre)
 #   @genre = genre
 # end
-# 
+#
 # def genre
 #   @genre
 # end
